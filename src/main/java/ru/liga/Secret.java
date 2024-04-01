7c1a1e0450331ce1c3d9be3ab5f4e849843e30b2
Arkanium77 <arkanium77@gmail.com>
2024-04-01 20:50:52 +0300

    initial commit

---
+++ src/main/java/ru/liga/Secret.java
@@ -0,0 +1,24 @@
+package ru.liga;
+
+@SuppressWarnings("ALL")
+public class Secret {
+    private final String branch1;
+    private final String branch2;
+
+    public Secret(String branch1, String branch2) {
+        this.branch1 = branch1;
+        this.branch2 = branch2;
+    }
+
+    public String generate() {
+        return branch1Secret() + branch2Secret();
+    }
+
+    private String branch1Secret() {
+        return "none";
+    }
+
+    private String branch2Secret() {
+        return "none";
+    }
+}
